cfceabd79afbb7acc3f5a3929c53a719d36c4b86
ParhusipC <carlosparhusip03@gmail.com>
2024-10-21 18:37:30 +0800

    update

---
+++ PTI Website/datakendaraan.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="styledash.css">
+</head>
+
+<header>
+<div class="sidebar">
+        <ul>
+            <h2>SIVASAK</h2>
+            <li><img src="home.png" alt=""><a href="#">Dashboard</a></li>
+            <li><img src="pb.png" alt=""> <a href="#">Buat Tawaran</a></li>
+            <li><img src="cb.png" alt=""><a href="#">Seleksi Vendor</a></li>
+            <li><img src="sh.png" alt=""><a href="#">Negoisasi</a></li>
+            <li><img src="undo.png" alt=""><a href="#">Kontrol Tender</a></li>
+            <li><img src="as.png" alt=""> <a href="#">Buat Akun Supir</a></li>
+            <li><img src="file.png" alt=""> <a href="#">Data Kendaraan</a></li>
+            <li><img src="alert.png" alt=""> <a href="#">Daftar Komplain</a></li>
+        </ul>
+    </div>
+</header>
+
+<body>
+<div class="navbar">
+        <div class="menu">
+            <ul>
+                <li><a href="#">Admin</a><img src="bp.png" alt=""></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
